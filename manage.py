3ce1d545ab9c2f21e7a372043f1d91825a8ab59b
Brayan <brayan.057@live.cl>
2020-11-05 02:06:48 -0300

    inicio deploy30

--- manage.py
+++ manage.py
@@ -6,8 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'facilito_store.settings.local')
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'facilito_store.settings.production')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'facilito_store.settings.local')
+    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'facilito_store.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
